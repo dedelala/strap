7878ca5bb84d57498c5c23ca58351f6c177edb1b
Dede Lamb <hey@dedela.la>
2021-10-05 18:02:39 +1100

    add init.sh

---
+++ init.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+##Usage of init.sh:
+##  init.sh <module-path>
+##
+##  Reinitializes this repository from scratch using your new module path and
+##  pushes an initial commit. Assumes the git url is ssh and an appropriately
+##  named bare repository exists. Probably only works with github or gitlab.
+##  Feel free to use it or don't. Removes itself on completion.
+
+module=$1
+
+if [[ -z "$module" ]]; then
+        awk -F'#' '/^##/{print $3}' "$0"
+        exit 2
+fi
+
+project=$(basename "$module")
+git_url="git@$(sed 's#/#:#' <<< "$module").git"
+
+printf "module: %s\nproject: %s\ngit_url: %s\n...continue? [y/n] " \
+        "$module" "$project" "$git_url"
+
+read -r confirm
+if [[ $confirm != "y" ]]; then
+        printf "will not continue, bye\n"
+        exit 1
+fi
+
+rm -rf .git
+git init --initial-branch=main
+git remote add origin "$git_url"
+
+go mod edit -module "$module"
+
+sed -i"" -e "s/strap/$project/g" .gitignore index.html
+
+cat > README.md <<< "# $project"
+
+git add .gitignore README.md go.mod *.go index.html static/
+git commit -m "initialized from github.com/dedelala/strap"
+git push -u origin main
+
+printf "%s successfully initialized\n" "$module"
+rm "$0"
